60aa8d902d2463557ccb8f6cb3a8b9fdca083875
saranya0648 <dellsaranya296@gmail.com>
2026-08-11 20:44:06 +0700

    Improve output formatting in min_max.sh

--- min_max.sh
+++ min_max.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
-echo "---------------------------------"
+
+echo "----------------------------------------"
 echo "Enter first number"
 read num1
 echo "Enter second number"
 read num2
 echo "Enter third number"
 read num3
-echo "--------------------------------"
+echo "----------------------------------------"
 
 max=$num1
 min=$num1
+
 if [ $num2 -gt $max ]; then
-  max=$num2
+    max=$num2
 fi
 if [ $num3 -gt $max ]; then
-  max=$num3
+    max=$num3
 fi
 
 if [ $num2 -lt $min ]; then
-  min=$num2
+    min=$num2
 fi
 if [ $num3 -lt $min ]; then
-  min=$num3
+    min=$num3
 fi
 
-echo "maximum Number=$max"
-echo "minimum Number=$min"
-echo "------------------------------"
+echo "Maximum Number=$max"
+echo "Minimum Number =$min"
+echo "----------------------------------------"
