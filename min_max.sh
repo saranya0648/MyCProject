1b622c3f426d34f7df6c047fce7c7daec32f6aec
saranya0648 <dellsaranya296@gmail.com>
2026-08-11 20:22:29 +0700

    Add min_max.sh to find max and min of three numbers

---
+++ min_max.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "---------------------------------"
+echo "Enter first number"
+read num1
+echo "Enter second number"
+read num2
+echo "Enter third number"
+read num3
+echo "--------------------------------"
+
+max = $num1
+min = $num1
+if[ $num2 -gf $max ]; then
+  max=$num2
+fi
+if[ $num3 -gf $max ]; then
+  max=$num3
+fi
+
+if [ $num2 -lt $min ]; then
+  min=$num2
+fi
+if [ $num3 -lt $min ]; then
+  min=$num3
+fi
+
+echo "maximum Number=$max"
+echo "minimum Number=$min"
+echo "------------------------------"
